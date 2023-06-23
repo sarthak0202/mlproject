625f290542b1625a8f306f8583c44ed02131abdf
Sarthak Vijayvargiya <sarthakvijayvargiya@Sarthaks-MacBook-Pro.local>
2023-06-23 16:12:06 -0700

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logs_path=os.path.join(os.getcwd(),"logs","LOG_FILE")
+
+os.makedirs(logs_path,exist_ok=True)
+
+LOGS_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+
+logging.basicConfig(
+     filename=LOGS_FILE_PATH,
+     level=logging.INFO, 
+     format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+    
+ )
+
+# if __name__=="__main__":
+#     logging.info("Logging has started")

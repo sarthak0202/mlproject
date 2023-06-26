e8b5a6a8007ed979ad6b3b3900bd763d96524dbc
Sarthak Vijayvargiya <sarthakvijayvargiya@Sarthaks-MacBook-Pro.local>
2023-06-26 12:23:25 -0700

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,9 +9,9 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str= os.join('artifacts',"train.csv")
-    test_data_path: str= os.join('artifacts',"test.csv")
-    raw_data_path: str= os.join('artifacts',"raw.csv")
+    train_data_path: str= os.path.join('artifacts',"train.csv")
+    test_data_path: str= os.path.join('artifacts',"test.csv")
+    raw_data_path: str= os.path.join('artifacts',"raw.csv")
 
 
 class DataIngsetion:
@@ -24,7 +24,7 @@ class DataIngsetion:
             df=pd.read_csv('notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
 
-            os.makedir(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("Trian Test split initiated")
